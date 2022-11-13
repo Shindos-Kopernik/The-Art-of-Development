b519a2ce20edd66eaaf5a4a06cfc3877e2a6fc6d
shindos <shindos@internet.ru>
2022-11-13 22:00:53 +0300

    Les5 8-29 minute

---
+++ The Art of Development/Lessons_Golang2_Advanced_Playlist2/Playlist2_Lesson5_MongoDB_continuation/internal/user/db/mongodb.go
@@ -0,0 +1,117 @@
+package db
+
+import (
+	"Lesson1_Rest_API/internal/user"
+	"Lesson1_Rest_API/pkg/logging"
+	"context"
+	"errors"
+	"fmt"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type db struct {
+	collection *mongo.Collection
+	logger     *logging.Logger
+}
+
+func (d db) Create(ctx context.Context, user user.User) (string, error) {
+	d.logger.Debug("create user")
+	result, err := d.collection.InsertOne(ctx, user)
+	if err != nil {
+		return "", fmt.Errorf("failed to create user due to error: %v", err)
+	}
+	d.logger.Debug("convert InsertedID to ObjectID")
+	oid, ok := result.InsertedID.(primitive.ObjectID)
+	if ok {
+		return oid.Hex(), nil
+	}
+	d.logger.Trace(user)
+	return "", fmt.Errorf("failed to convert object id to hex. probably oid: %s", oid)
+}
+
+func (d db) FindOne(ctx context.Context, id string) (u user.User, err error) {
+	oid, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return u, fmt.Errorf("failed to convert hex objectid. hex %s", id)
+	}
+
+	filter := bson.M{"_id": oid}
+
+	result := d.collection.FindOne(ctx, filter)
+	if result.Err() != nil {
+		if errors.Is(result.Err(), mongo.ErrNoDocuments) {
+			// TODO ErrEntityNotFound
+			return u, fmt.Errorf("not found")
+		}
+		return u, fmt.Errorf("failed to find one user by id: %s due to error: %v", id, err)
+	}
+	if err = result.Decode(&u); err != nil {
+		return u, fmt.Errorf("failed to decode user(id:%s) from DB due to error: %v", id, err)
+	}
+	return u, nil
+}
+
+func (d db) Update(ctx context.Context, user user.User) error {
+	objectID, err := primitive.ObjectIDFromHex(user.ID)
+	if err != nil {
+		return fmt.Errorf("failed to convert userID to ObjectID: id=%s", user.ID)
+	}
+
+	filter := bson.M{"_id": objectID}
+
+	userBytes, err := bson.Marshal(user)
+	if err != nil {
+		return fmt.Errorf("failed to marshal user. error: %v", err)
+	}
+	// {"id":122412412}
+	var updateUserObj bson.M
+	err = bson.Unmarshal(userBytes, &updateUserObj)
+	if err != nil {
+		return fmt.Errorf("failed to unmarshal user bytes. error: %v", err)
+	}
+	delete(updateUserObj, "_id")
+
+	update := bson.M{
+		"$set": updateUserObj, // "$set" -- это переменная, которая отдается на вход mongoDB
+	}
+
+	result, err := d.collection.UpdateOne(ctx, filter, update)
+	if err != nil {
+		return fmt.Errorf("failed to execute update user query. error: %v", err)
+	}
+	if result.MatchedCount == 0 { // MatchedCount --нашли ли мы, что-нибудь, т.к. mongo сперва ищет сущность,
+		// TODO ErrEntityNotFound     // а затем её обновляет
+		return fmt.Errorf("not found")
+	}
+	d.logger.Tracef("Matched %d documents and Modified %d documents", result.MatchedCount, result.ModifiedCount)
+	return nil
+}
+
+func (d db) Delete(ctx context.Context, id string) error {
+	objectID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return fmt.Errorf("failed to convert userID to ObjectID: id=%s", id)
+	}
+	filter := bson.M{"_id": objectID}
+
+	result, err := d.collection.DeleteOne(ctx, filter)
+	if err != nil {
+		return fmt.Errorf("faiked to execude query. error: %v", err)
+	}
+	if result.DeletedCount == 0 {
+		// TODO ErrEntityNotFound
+		return fmt.Errorf("not found")
+	}
+	d.logger.Tracef("Deleted %d documents ", result.DeletedCount)
+
+	return nil
+}
+
+func NewStorage(database *mongo.Database, collection string, logger *logging.Logger) user.Storage {
+	return &db{
+		collection: database.Collection(collection),
+		logger:     logger,
+	}
+}
